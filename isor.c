ef4fb3361b9836bafa239f257ef4f8d68b865622
aditi-28-11 <adinaduthota@gmail.com>
2025-04-15 02:39:02 +0530

    Update isor.c
    a proper functioning insertion sort in linked list using a single pointer

--- isor.c
+++ isor.c
@@ -1,43 +1,87 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct colgate *m,*p, *f,*r,*t,*n;
-void isort()
+int t[100],valid;
+typedef struct mylist {
+	int data;
+	struct mylist* next;
+} *list;
+list f=NULL;
+list createlist(int value) { // memory is allocated to each node respectively here
+	list node;
+	node=(list)malloc(sizeof (struct mylist));
+	node->data=value;
+	node->next= NULL;
+	return node;
+}
+void insert(int value)
 {
-  p=f;
-  m=f;
-    for (t=f;t;t=t->next){
-      for (n=f;n;n=n->next){
-        if( n->data < t->data){
-           if (t==f){
-            p->next=n->next;
-            n->next=t;
-            f=n;
-            n=p;
-           }
-          else if (n==r){
-            p->next=NULL;
-            n->next=t;
-            r=p;
-            m->next=n;
-          }
-          else if (t==f && n==r){
-            p->next=NULL;
-            n->next=t;
-            f=n;
-            r=p;
-          }
-          else (t!=f && t!=r){
-             p->next=n->next;
-             n->next=t;
-             m->next=n;
-          }
-        }
-        p=n;
-        m=t;
-      }
-    }
+	list temp,rag,prev=f;
+	temp=createlist(value);
+	if (f==NULL) {
+		f=temp;
+	}
+	else if (temp->data < f->data) //front insertion
+	{
+		temp->next=f;
+		f=temp;
+	}
+	else if (f->next==NULL) //second element
+	{
+		f->next=temp;
+	}
+	else
+	{
+		int x=0;
+		for(rag=f; rag!=NULL; rag=rag->next)
+		{
+			if(temp->data<rag->data)
+			{
+				temp->next=prev->next;
+				prev->next=temp;
+				x=1;
+				break;
+			}
+			prev=rag;
+		}
+		if (x==0) {
+			prev->next=temp;
+		}
+	}
 }
 
+int verification() {
+	list temp=f;
+	while (temp->next)
+	{
+		if (temp->data>temp->next->data) {
+			return 0;
+		}
+		return 1;
+	}
+}
+int main()
+{
+	int n,i,v;
+	int value;
+	list temp;
+	printf("enter the number of elements to be inserted\n");
+	scanf("%d",&n);
+	for (i=0; i<n; i++)
+	{
+		value=(rand()%50);
+		printf("value %d\t",value);
+		insert(value);
+		v= verification();
+		printf("verification %d\n",v);
+	}
+	valid=i;
+	printf("sorted array elements are\n");
+	for (temp=f; temp->next!=NULL; temp=temp->next)
+	{
+		printf("%d \t",temp->data);
+	}
+	printf("%d",temp->data);
+}
 
 
 
